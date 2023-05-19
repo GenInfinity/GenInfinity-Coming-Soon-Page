4aaf081c2cd2f1abc99bd8f4fd2e9894e3f05f5c
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-19 17:27:08 +0100

    fixed styling for mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,40 @@ import { ReactComponent as Discord } from "./assets/svg/Discord.svg";
 import { ReactComponent as Facebook } from "./assets/svg/Facebook.svg";
 import { ReactComponent as Telegram } from "./assets/svg/Telegram.svg";
 import Moon from "./assets/images/Moon.png"
+import GenInfinity from "./assets/images/GenInfinity.png"
 
 const App = () => {
 
   return (
-    <div className="bg-[url('./assets/images/Star-Light.gif')] h-[100vh] bg-center bg-no-repeat bg-cover flex flex-col justify-center px-24">
-      <div className="flex justify-end mb-5">
+    <div className="bg-[url('./assets/images/Star-Light.gif')] h-[100vh] bg-center bg-no-repeat bg-cover flex flex-col justify-center px-4 lg:px-24">
+      <div className="hidden md:flex lg:flex justify-end lg:mb-5">
         <img src={Moon} alt=""/>
       </div>
-      <h1 className="text-6xl font-bold mb-8 text-center">
+      <div className="flex md:hidden lg:hidden justify-center lg:mb-5">
+        <img src={GenInfinity} alt=""/>
+      </div>
+      <h1 className="text-3xl md:text-6xl font-bold mb-8 text-center">
         <span className="text-[#3E918D] font-['Clash_Display']">Gen</span><span className="text-[#3FFEF7] font-NeueMontrealBold">Infinity</span>
       </h1>
-      <h2 className="text-white text-9xl mb-8 font-NeueMontrealBold font-bold text-center">
+      <h2 className="text-white text-6xl md:text-9xl mb-8 font-NeueMontrealBold font-bold text-center">
         Coming Soon
       </h2>
-      <p className="font-bold text-white text-center mb-11  font-NeueMontrealRegular">
-      Gen Infinity is a DAO-based NFT project, that gives the community complete control on how the project should be ran and how its fund should be spent
+      <p className="font-bold text-white text-center mb-11 font-NeueMontrealRegular">
+        Gen Infinity is a DAO-based NFT project, that gives the community complete control on how the project should be ran and how its fund should be spent
       </p>
       <div className="flex justify-center gap-16">
-        <Telegram/>
-        <Discord/>
-        <Twitter/>
-        <Facebook/>
+        <a href="https://t.me/GenInfinity" target="_blank" rel="noopener noreferrer">
+          <Telegram/>
+        </a>
+        <a href="https://discord.gg/kbakBT54jr" target="_blank" rel="noopener noreferrer">
+            <Discord/>
+        </a>
+        <a href="https://twitter.com/GenInfinityNFT" target="_blank" rel="noopener noreferrer">
+          <Twitter/>
+        </a>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <Facebook/>
+        </a>    
       </div>
     </div>
   )
